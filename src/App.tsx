4cd335b78da2d162e0948c1fa0a82d5db23f31d8
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-11-23 16:20:01 +0530

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Button } from "@mui/material";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <AcUnitIcon />
     </div>
   );
-}
+};
 
 export default App;
